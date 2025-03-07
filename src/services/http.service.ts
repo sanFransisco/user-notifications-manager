4ced7702802571dd05dc6f8acd476ed3d7e3eeec
yali <yali@1mrobotics.com>
2025-03-08 00:21:30 +0200

    clean up

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -11,7 +11,6 @@ abstract class HttpService {
     this.axiosClient = client.getClient();
   }
 
-  // Generic function to send HTTP requests
   public async sendRequest(method: HttpMethod, url: string, body: any = null): Promise<AxiosResponse> {
     try {
       const options: any = {
@@ -19,12 +18,11 @@ abstract class HttpService {
         url,
       };
 
-      // If method is POST or PUT and body is provided, attach it
       if (body && (method === 'POST' || method === 'PUT')) {
         options.data = body;
       }
 
-      const res = await this.axiosClient(options); // Send the request
+      const res = await this.axiosClient(options);
       if (res.status == 429 || res.status > 500) throw new Error('Service cant answer the request');
       return res;
     } catch (error: any) {
@@ -33,25 +31,21 @@ abstract class HttpService {
     }
   }
 
-  // Method to send a GET request
   public async get(url: string, params?: any): Promise<any> {
     const response = await this.sendRequest('GET', url, params);
     return response.data;
   }
 
-  // Method to send a POST request
   public async post(url: string, body?: any): Promise<any> {
     const response = await this.sendRequest('POST', url, body);
     return response.data;
   }
 
-  // Method to send a PUT request
   public async put(url: string, body?: any): Promise<any> {
     const response = await this.sendRequest('PUT', url, body);
     return response.data;
   }
 
-  // Method to send a DELETE request
   public async delete(url: string, params?: any): Promise<any> {
     const response = await this.sendRequest('DELETE', url, params);
     return response.data;
